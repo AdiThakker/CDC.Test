e496527d9c77633287a6fc8b2a65b582b2fb275a
Aditya Thakker <Aditya.Thakker@gmail.com>
2019-10-21 21:24:33 -0400

    More scaffolding

--- CDC.Service/CustomerDataChangeDetector.cs
+++ CDC.Service/CustomerDataChangeDetector.cs
@@ -1,6 +1,8 @@
 ﻿using CDC.Service.Events;
+using CDC.Service.Utility;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace CDC.Service
@@ -8,5 +10,37 @@ namespace CDC.Service
     public class CustomerDataChangeDetector
     {
         public event EventHandler<CustomerCDCEventArgs<CustomerCDCEvent>> HandleCustomerCDCEvent;
+
+        private void RunStoredProcedure(string procedureName, Action<IDataReader> loadDataAction,
+            Dictionary<string, object> parameters = null)
+        {
+            using (var connection = CDCHelper.GetConnection(_config.ConnectionString))
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = procedureName;
+                command.CommandType = CommandType.StoredProcedure;
+
+                if (parameters != null)
+                    foreach (var p in parameters)
+                    {
+                        var param = command.Parameters.AddWithValue(p.Key, p.Value);
+                        if (p.Value is DataTable) param.SqlDbType = SqlDbType.Structured;
+                    }
+
+                connection.Open();
+
+                if (loadDataAction != null)
+                {
+                    var reader = command.ExecuteReader();
+
+                    loadDataAction(reader);
+                }
+                else
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
     }
 }
---
+++ CDC.Service/CustomerDataChangeProcessor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CDC.Service
+{
+    class CustomerDataChangeProcessor
+    {
+    }
+}
